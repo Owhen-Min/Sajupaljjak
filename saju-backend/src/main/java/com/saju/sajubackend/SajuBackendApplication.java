54f4c87956b8e1103b184f6ea419d4a088631e30
Mojin2 <aiduriaaa@gmail.com>
2025-01-25 14:53:39 +0900

    [CHORE] 초기 프로젝트 생성

---
+++ saju-backend/src/main/java/com/saju/sajubackend/SajuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saju.sajubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SajuBackendApplication.class, args);
+    }
+
+}
---
+++ saju-backend/src/test/java/com/saju/sajubackend/SajuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saju.sajubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SajuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
