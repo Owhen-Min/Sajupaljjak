dc41a98d8a91643b53a6df102ba64f7e6eedc187
Suyoung Oh <suyoungoh@Suyoungui-MacBookPro.local>
2025-01-29 15:23:59 +0900

    [TEST] prod 디버깅

--- saju-backend/src/main/java/com/saju/sajubackend/SajuBackendApplication.java
+++ saju-backend/src/main/java/com/saju/sajubackend/SajuBackendApplication.java
@@ -1,15 +1,26 @@
 package com.saju.sajubackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
-@EnableJpaAuditing
+import java.util.Arrays;
+
 @SpringBootApplication
 public class SajuBackendApplication {
 
     public static void main(String[] args) {
+        System.out.println("---------------------start-----------------");
         SpringApplication.run(SajuBackendApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner printActiveProfiles(Environment environment) {
+        return args -> {
+            System.out.println("✅ Active Profiles: " + Arrays.toString(environment.getActiveProfiles()));
+            System.out.println("🛠️ Current Property: " + environment.getProperty("spring.datasource.url")); // 예제용
+        };
+    }
 }
