ba2acd0a73a9f0f9fe8ece2dd5f89d356a9da1ef
오수영 <ohsusu515@gmail.com>
2025-02-03 17:16:54 +0900

    [FEAT] Redis Config 파일 작성

---
+++ saju-backend/src/main/java/com/saju/sajubackend/common/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.saju.sajubackend.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+	@Value("${spring.data.redis.host}")
+	private String host;
+
+	@Value("${spring.data.redis.port}")
+	private int port;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
+	}
+
+	@Bean
+	public StringRedisTemplate redisTemplate() {
+		return new StringRedisTemplate(redisConnectionFactory());
+	}
+}
