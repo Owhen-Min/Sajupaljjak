783b113a6b5a34cc69a217cfccee7f0f4f0e5fa2
조희주 <sus6an@naver.com>
2025-02-04 15:16:30 +0900

    #8 [FEAT] 궁합 상대를 당일 자정까지 캐시

--- saju-backend/src/main/java/com/saju/sajubackend/api/matching/service/MatchingService.java
+++ saju-backend/src/main/java/com/saju/sajubackend/api/matching/service/MatchingService.java
@@ -7,21 +7,49 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class MatchingService {
 
+    private final RedisTemplate<String, Object> redisTemplate;
     private final MatchingQueryDslRepository matchingQueryDslRepository;
 
     private final int MAGINOT_SCORE = 80;
 
     public List<Member> getMatchingMembers(Long memberId) {
-        // todo : redis에 이미 존재하는지 확인
+        // 1. redis에 이미 존재하는지 확인
+        if (redisTemplate.hasKey(String.valueOf(memberId))) return getCache(memberId);
 
         // 2. 랜덤으로 3명 가져오기
-        return matchingQueryDslRepository.findMatchingMembers(memberId, MAGINOT_SCORE);
+        List<Member> matchingMembers = matchingQueryDslRepository.findMatchingMembers(memberId, MAGINOT_SCORE);
+        createCache(memberId, matchingMembers);
+        return matchingMembers;
+    }
+
+    private List<Member> getCache(Long memberId) {
+        Object cachedData = redisTemplate.opsForValue().get(getRedisKey(memberId));
+        if (cachedData instanceof List<?>) {
+            return (List<Member>) cachedData;
+        }
+        return Collections.emptyList();
+    }
+
+    private void createCache(Long memberId, List<Member> matchingMembers) {
+        LocalDateTime midnight = LocalDate.now().plusDays(1).atStartOfDay();
+        long secondsUntilMidnight = ChronoUnit.SECONDS.between(LocalDateTime.now(), midnight);
+
+        redisTemplate.opsForValue().set(getRedisKey(memberId), matchingMembers, secondsUntilMidnight, TimeUnit.SECONDS);
+    }
+
+    private String getRedisKey(Long memberId) {
+        return "matching:" + memberId;
     }
 }
