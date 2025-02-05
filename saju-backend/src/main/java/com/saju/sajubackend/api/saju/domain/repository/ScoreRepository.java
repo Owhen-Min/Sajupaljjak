5ba3450608c61bf7f7220916784b8d73c51999a5
조희주 <sus6an@naver.com>
2025-02-05 17:51:36 +0900

    #8 [REFACTOR] 사용하지 않는 Repository 메서드 삭제

--- saju-backend/src/main/java/com/saju/sajubackend/api/saju/domain/repository/ScoreRepository.java
+++ saju-backend/src/main/java/com/saju/sajubackend/api/saju/domain/repository/ScoreRepository.java
@@ -1,15 +1,9 @@
 package com.saju.sajubackend.api.saju.domain.repository;
 
 import com.saju.sajubackend.api.saju.domain.Score;
-import com.saju.sajubackend.common.enums.CelestialStem;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ScoreRepository extends JpaRepository<Score, Long> {
-
-    @Query("SELECT s.score FROM Score s WHERE s.source = :source AND s.target = :target")
-    Long findScoreByCelestialStem(@Param("source") CelestialStem source, @Param("target") CelestialStem target);
 }
 
 
