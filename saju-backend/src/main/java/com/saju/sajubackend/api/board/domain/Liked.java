dc97e5f92e667c74cc50f9ddb75f79fa1087688c
오수영 <ohsusu515@gmail.com>
2025-02-06 12:49:18 +0900

    [FIX] like 예약어로 인한 테이블 이름 변경 (like -> liked)

--- saju-backend/src/main/java/com/saju/sajubackend/api/board/domain/Liked.java
+++ saju-backend/src/main/java/com/saju/sajubackend/api/board/domain/Liked.java
@@ -1,16 +1,8 @@
 package com.saju.sajubackend.api.board.domain;
 
-import com.saju.sajubackend.common.entity.BaseTimeEntity;
 import com.saju.sajubackend.api.member.domain.Member;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import com.saju.sajubackend.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "LIKE")
+@Table(name = "LIKED")
 public class Liked extends BaseTimeEntity {
 
     @Id
