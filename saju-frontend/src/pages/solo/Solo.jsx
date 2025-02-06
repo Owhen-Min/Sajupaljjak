4dce385d83fe065b691529bbc8defc793201c5ab
Lee <dlwhdans98@naver.com>
2025-02-06 09:35:43 +0900

    [FIX] 에러 수정

--- saju-frontend/src/pages/solo/Solo.jsx
+++ saju-frontend/src/pages/solo/Solo.jsx
@@ -1,27 +1,28 @@
 import BottomNav from "../../components/BottomNav";
 import TopBar from "../../components/TopBar";
 import UserList from "../../components/UserList";
-import useFetchData from '../../hooks/useFetchData';
+// import useFetchData from '../../hooks/useFetchData';
+import { testUsers } from "../../data/user";
 
 function Solo() {
-  const { data: users, isLoading, isError } = useFetchData('/api/match/top', ['match', 'top']);
+  // const { data: users, isLoading, isError } = useFetchData('/api/match/top', ['match', 'top']);
   
-  if (users){
-    console.log(users);
-  }
+  // if (users){
+  //   console.log(users);
+  // }
 
-  if (isLoading) {
-    return <div>로딩중</div>;
-  }
+  // if (isLoading) {
+  //   return <div>로딩중</div>;
+  // }
 
-  if (isError) {
-    return <div>에러</div>;
-  }
+  // if (isError) {
+  //   return <div>에러</div>;
+  // }
 
   return (
     <div>
       <TopBar />
-      <UserList users={users} />
+      <UserList users={testUsers} />
       <BottomNav />
     </div>
   );
