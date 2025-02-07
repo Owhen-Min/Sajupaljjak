dfe06aaf1e6ec463690bfcc1520b16d0eedfa55b
윤수정 <clftkdh01@gmail.com>
2025-02-07 10:14:55 +0900

    Update AuthController.java

--- saju-backend/src/main/java/com/saju/sajubackend/api/auth/controller/AuthController.java
+++ saju-backend/src/main/java/com/saju/sajubackend/api/auth/controller/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("api/auth")
+@RequestMapping("/api/auth")
 @RestController
 public class AuthController {
 
