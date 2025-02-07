a88067884ae5e527ee50cb08724ef6e046b02f6b
조희주 <sus6an@naver.com>
2025-02-07 18:46:39 +0900

    #12 [REFACTOR] MatchingRedisUtil 별도의 클래스로 분리

--- saju-backend/src/main/java/com/saju/sajubackend/common/util/MatchingRedisUtil.java
+++ saju-backend/src/main/java/com/saju/sajubackend/common/util/MatchingRedisUtil.java
@@ -1,2 +1,40 @@
-package com.saju.sajubackend.common.util;public class MatchingRedisUtil {
+package com.saju.sajubackend.common.util;
+
+import com.saju.sajubackend.api.matching.dto.MemberListResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class MatchingRedisUtil {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public List<MemberListResponseDto> getCache(Long memberId) {
+        Object cachedData = redisTemplate.opsForValue().get(getRedisKey(memberId));
+        if (cachedData instanceof List<?>) {
+            return (List<MemberListResponseDto>) cachedData;
+        }
+        return Collections.emptyList();
+    }
+
+    public void createCache(Long memberId, List<MemberListResponseDto> matchingMembers) {
+        LocalDateTime midnight = LocalDate.now().plusDays(1).atStartOfDay();
+        long secondsUntilMidnight = ChronoUnit.SECONDS.between(LocalDateTime.now(), midnight);
+
+        redisTemplate.opsForValue().set(getRedisKey(memberId), matchingMembers, secondsUntilMidnight, TimeUnit.SECONDS);
+    }
+
+    private String getRedisKey(Long memberId) {
+        return "matching:" + memberId;
+    }
+
 }
