b7f9bc4c86ca8b0ae407cf01016d8d23d5a24485
오수영 <ohsusu515@gmail.com>
2025-02-11 03:23:18 +0900

    [INFRA] test 주석

--- saju-backend/src/test/java/com/saju/sajubackend/SajuBackendApplicationTests.java
+++ saju-backend/src/test/java/com/saju/sajubackend/SajuBackendApplicationTests.java
@@ -1,13 +1,10 @@
 package com.saju.sajubackend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SajuBackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class SajuBackendApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
