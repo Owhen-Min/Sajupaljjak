a9c7c6d79d87f49a37500d72d80c774655c0666f
조희주 <sus6an@naver.com>
2025-02-10 15:53:52 +0900

    #18 [chore] 불필요한 메서드 제거

--- saju-backend/src/main/java/com/saju/sajubackend/api/matching/dto/MatchingProfileResponseDto.java
+++ saju-backend/src/main/java/com/saju/sajubackend/api/matching/dto/MatchingProfileResponseDto.java
@@ -1,9 +1,5 @@
 package com.saju.sajubackend.api.matching.dto;
 
-import com.saju.sajubackend.api.couple.domain.CoupleYear;
-import com.saju.sajubackend.api.member.domain.Member;
-import com.saju.sajubackend.api.saju.domain.Saju;
-
 public record MatchingProfileResponseDto(
 
         long id,
@@ -24,19 +20,4 @@ public record MatchingProfileResponseDto(
         String bad,
         String advice
 ) {
-
-    public static MatchingProfileResponseDto fromEntity(Member matchingMember, Integer score, Saju saju, CoupleYear coupleYear) {
-        return null;
-    }
-
-    private static String formatContent(CoupleYear coupleYear) {
-        return String.format(
-                "두 사람의 궁합: %s\n케미: %s\n장점: %s\n단점: %s\n조언: %s",
-                coupleYear.getHarmony(),
-                coupleYear.getChemi(),
-                coupleYear.getGood(),
-                coupleYear.getBad(),
-                coupleYear.getAdvice()
-        );
-    }
 }
