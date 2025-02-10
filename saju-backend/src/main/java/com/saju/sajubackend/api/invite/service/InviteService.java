a783686de00368ed4da8adc7053f498172cdebe3
오수영 <ohsusu515@gmail.com>
2025-02-10 17:07:08 +0900

    #14 [REFACTOR] 클래스명 변경 (-> InviteRedisUtil, @Component사용)

--- saju-backend/src/main/java/com/saju/sajubackend/api/invite/service/InviteService.java
+++ saju-backend/src/main/java/com/saju/sajubackend/api/invite/service/InviteService.java
@@ -3,7 +3,6 @@ package com.saju.sajubackend.api.invite.service;
 import com.saju.sajubackend.api.couple.domain.Couple;
 import com.saju.sajubackend.api.couple.repository.CoupleRepository;
 import com.saju.sajubackend.api.invite.dto.InviteCreateResponseDto;
-import com.saju.sajubackend.api.invite.repository.InviteRepository;
 import com.saju.sajubackend.api.member.domain.Member;
 import com.saju.sajubackend.api.member.repository.MemberRepository;
 import com.saju.sajubackend.common.enums.Gender;
@@ -11,6 +10,7 @@ import com.saju.sajubackend.common.enums.RelationshipStatus;
 import com.saju.sajubackend.common.exception.BadRequestException;
 import com.saju.sajubackend.common.exception.BaseException;
 import com.saju.sajubackend.common.exception.ErrorMessage;
+import com.saju.sajubackend.common.util.InviteRedisUtil;
 import com.saju.sajubackend.common.util.RandomUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,25 +22,25 @@ import java.time.LocalDateTime;
 @Service
 @RequiredArgsConstructor
 public class InviteService {
-    private final InviteRepository inviteRepository;
+    private final InviteRedisUtil inviteRedisUtil;
     private final MemberRepository memberRepository;
     private final CoupleRepository coupleRepository;
 
     @Transactional
     public InviteCreateResponseDto createInviteCode(Long memberId) {
-        String code = inviteRepository.findCodeByMemberId(memberId)
+        String code = inviteRedisUtil.findCodeByMemberId(memberId)
                 .orElseGet(() -> {
                     String newCode = RandomUtil.generateRandomCode(8);
-                    inviteRepository.saveInvitationCode(memberId, newCode);
+                    inviteRedisUtil.saveInvitationCode(memberId, newCode);
                     return newCode;
                 });
-        Long ttl = inviteRepository.getTTL(memberId);
+        Long ttl = inviteRedisUtil.getTTL(memberId);
         return new InviteCreateResponseDto(code, ttl);
     }
 
     @Transactional
     public void createCouple(Long joinerId, String inviterCode, LocalDateTime startDate) {
-        Long inviterId = inviteRepository.findMemberIdByCode(inviterCode)
+        Long inviterId = inviteRedisUtil.findMemberIdByCode(inviterCode)
                 .orElseThrow(() -> new BadRequestException(ErrorMessage.INVITE_CODE_NOT_FOUND));
 
         Member inviter = memberRepository.findById(inviterId)
@@ -63,7 +63,7 @@ public class InviteService {
 
         coupleRepository.save(couple);
 
-        inviteRepository.deleteBothCode(inviterId, joinerId);
+        inviteRedisUtil.deleteBothCode(inviterId, joinerId);
     }
 
     private boolean isValidGenderPair(Member inviter, Member joiner) {
