9a431f206890a20a20c1c84117975eb6a9d4df5e
조희주 <sus6an@naver.com>
2025-02-10 16:01:32 +0900

    #18 [refactor] 사용하지 않는 메서드 제거

--- saju-backend/src/main/java/com/saju/sajubackend/api/couple/repository/CoupleYearRepository.java
+++ saju-backend/src/main/java/com/saju/sajubackend/api/couple/repository/CoupleYearRepository.java
@@ -3,9 +3,6 @@ package com.saju.sajubackend.api.couple.repository;
 import com.saju.sajubackend.api.couple.domain.CoupleYear;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CoupleYearRepository extends JpaRepository<CoupleYear, Long> {
 
-    Optional<CoupleYear> findByMaleAndFemale(String male, String female);
 }
