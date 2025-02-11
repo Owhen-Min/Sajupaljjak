e8c4b36b1372cc723ac311490ea961ef832d69a3
조희주 <sus6an@naver.com>
2025-02-11 20:37:28 +0900

    #21 [CHORE] Redis Stream 관련 설정 추가

---
+++ saju-backend/src/main/java/com/saju/sajubackend/common/config/MongoConfig.java
@@ -0,0 +1,2 @@
+package com.saju.sajubackend.common.config;public class MongoConfig {
+}
--- saju-backend/src/main/java/com/saju/sajubackend/common/config/RedisConfig.java
+++ saju-backend/src/main/java/com/saju/sajubackend/common/config/RedisConfig.java
@@ -6,14 +6,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.stream.StreamMessageListenerContainer;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
+
     @Value("${spring.data.redis.host}")
     private String host;
 
@@ -34,4 +40,19 @@ public class RedisConfig {
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
+
+    @Bean
+    public StreamMessageListenerContainer<String, MapRecord<String, String, String>> streamMessageListenerContainer( // 메시지 리스너
+                                                                                                                     RedisConnectionFactory redisConnectionFactory) {
+
+        StreamMessageListenerContainer.StreamMessageListenerContainerOptions<String, MapRecord<String, String, String>> options =
+                StreamMessageListenerContainer.StreamMessageListenerContainerOptions.builder()
+                        .batchSize(10) // 한 번에 10개씩 메시지 읽기
+                        .pollTimeout(Duration.ofMillis(100)) // 100ms 대기 후 읽기
+                        .executor(Executors.newFixedThreadPool(4)) // 4개의 스레드 사용
+                        .build();
+
+        return StreamMessageListenerContainer.create(redisConnectionFactory, options);
+    }
+
 }
