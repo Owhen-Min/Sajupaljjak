0e3c128705f1cb497f3ddcd0097dab35a79406c0
조희주 <sus6an@naver.com>
2025-02-11 20:37:44 +0900

    #21 [CHORE] MongoDB 관련 설정 추가

--- saju-backend/src/main/java/com/saju/sajubackend/common/config/MongoConfig.java
+++ saju-backend/src/main/java/com/saju/sajubackend/common/config/MongoConfig.java
@@ -1,2 +1,32 @@
-package com.saju.sajubackend.common.config;public class MongoConfig {
+package com.saju.sajubackend.common.config;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@Configuration
+@EnableReactiveMongoRepositories(basePackages = "com.saju.sajubackend.api.chat.repository")
+public class MongoConfig extends AbstractReactiveMongoConfiguration {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    @Override
+    @Bean
+    public MongoClient reactiveMongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
 }
+
