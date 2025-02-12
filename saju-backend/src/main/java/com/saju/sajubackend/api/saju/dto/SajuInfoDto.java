058bf569e586641d61a9bf648493072ff951b73c
Mojin2 <aiduriaaa@gmail.com>
2025-02-12 17:03:57 +0900

    #25 [REFACTOR] record에 JsonProperty추가

--- saju-backend/src/main/java/com/saju/sajubackend/api/saju/dto/SajuInfoDto.java
+++ saju-backend/src/main/java/com/saju/sajubackend/api/saju/dto/SajuInfoDto.java
@@ -1,8 +1,14 @@
 package com.saju.sajubackend.api.saju.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record SajuInfoDto (
+    @JsonProperty(value="date")
     String daily,
+    @JsonProperty(value="month")
     String monthly,
+    @JsonProperty(value="time")
     String timely,
+    @JsonProperty(value="year")
     String yearly
 ){}
