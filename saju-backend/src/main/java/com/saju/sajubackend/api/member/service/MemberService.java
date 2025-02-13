9a4667eba5495dec7e97d723bb404156e8076653
Mojin2 <aiduriaaa@gmail.com>
2025-02-13 20:38:29 +0900

    [REFACTOR] 코드리뷰 반영

--- saju-backend/src/main/java/com/saju/sajubackend/api/member/service/MemberService.java
+++ saju-backend/src/main/java/com/saju/sajubackend/api/member/service/MemberService.java
@@ -6,22 +6,22 @@ import com.saju.sajubackend.api.member.repository.MemberRepository;
 import com.saju.sajubackend.common.enums.DrinkingFrequency;
 import com.saju.sajubackend.common.enums.Religion;
 import com.saju.sajubackend.common.enums.SmokingStatus;
+import com.saju.sajubackend.common.exception.ErrorMessage;
+import com.saju.sajubackend.common.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
     // 예제에서는 id가 1인 회원을 조회합니다.
     public Member getMember(Long memberId) {
         return memberRepository.findById(4L)
-                .orElseThrow(() -> new RuntimeException("Member not found"));
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.MEMBER_NOT_FOUND));
     }
 
     public void updateMemberFilter(Long memberId, MemberFilterRequest request) {
