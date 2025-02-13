9cf8a65911456b0c5644f869819ba6181865fca3
Mojin2 <aiduriaaa@gmail.com>
2025-02-13 20:19:01 +0900

    #27 [FEAT] 솔로페이지 구현

---
+++ saju-frontend/src/components/CustomCarousel.jsx
@@ -0,0 +1,44 @@
+import Carousel from "react-spring-3d-carousel";
+import { useState, useEffect } from "react";
+import { config } from "react-spring";
+
+export default function SquareCarousel() {
+  const [goToSlide, setGoToSlide] = useState(0);
+
+  const slides = Array.from({ length: 3 }, (_, index) => ({
+    key: index,
+    content: (
+      <div
+        class="w-48 h-48 mt-5 flex items-center justify-center text-white bg-red-400 text-2xl font-bold rounded-xl cursor-pointer "
+        onClick={() => setGoToSlide(index)}
+      >
+        {/* <div class="relative group">
+          <div class="w-52 h-52 absolute bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-100 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+          <div class="w-52 h-52 cursor-pointer relative px-7 py-7 bg-slate-100 ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+            <div class="flex-full h-full items-center justify-center"></div>
+          </div>
+        </div> */}
+        사용자 추천 {index + 1}
+      </div>
+    ),
+  }));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setGoToSlide((prev) => (prev + 1) % slides.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [slides.length]);
+
+  return (
+    <div className="w-[90%] h-64 mx-auto flex ">
+      <Carousel
+        slides={slides}
+        goToSlide={goToSlide}
+        offsetRadius={3} // 간격을 조금 넓혀 절반 정도 보이게 설정
+        showNavigation={false}
+        animationConfig={config.gentle}
+      />
+    </div>
+  );
+}
--- saju-frontend/src/pages/match/Match.jsx
+++ saju-frontend/src/pages/match/Match.jsx
@@ -3,27 +3,56 @@ import BottomNav from "../../components/BottomNav";
 import TopBar from "../../components/TopBar";
 import UserList2 from "../../components/UserList2";
 import { useInfiniteGet } from "../../hooks/useInfiniteGet";
+import AnimationHeartLoader from "../../components/AnimationHeartLoader";
+import { TypeAnimation } from "react-type-animation";
 
 function Match() {
-  const { data, fetchNextPage, hasNextPage, error, isLoading } = useInfiniteGet("/api/match");
+  const { data, fetchNextPage, hasNextPage, error, isLoading } =
+    useInfiniteGet("/api/match");
 
   console.log(data);
-  if (isLoading) return <div>로딩중 ...</div>;  
+  if (isLoading)
+    return (
+      <div className="flex w-full h-screen items-center justify-center flex-col bg-red-100 gap-y-4">
+        <AnimationHeartLoader />
+        <TypeAnimation
+          sequence={[
+            1000,
+            "매칭 상대방 검색중...", // Types 'One'
+            20000, // Waits 1s
+            // "매칭 상대방을 검색중 입니다", // Deletes 'One' and types 'Two'
+            // 2000, // Waits 2s
+            // "잠시만 기다려주세요",
+            // 20000, // Types 'Three' without deleting 'Two'
+            () => {
+              console.log("Sequence completed");
+            },
+          ]}
+          wrapper="span"
+          cursor={true}
+          repeat={Infinity}
+          style={{
+            color: "#ff7270",
+            fontWeight: 700,
+            fontSize: "16px",
+            display: "inline-block",
+          }}
+        />
+      </div>
+    );
   if (error) return <div>에러 : {error.message}</div>;
 
-  const handleScroll = () =>{
-
-  };  
+  const handleScroll = () => {};
   // useEffect(() => {
   //   window.addEventListener("scroll", handleScroll);
   //   return () => window.removeEventListener("scroll", handleScroll);
   // }, [isLoading]);
 
-
   return (
     <div>
       <TopBar />
-      <UserList2 users={data} />
+      {/* {isLoading ? <MagnifyingGlass /> : <UserList2 users={data} />} */}
+
       <BottomNav />
     </div>
   );
