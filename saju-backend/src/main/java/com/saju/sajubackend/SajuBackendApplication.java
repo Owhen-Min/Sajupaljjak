3761622c60bceda093d71510de619997e55bf2a3
오수영 <ohsusu515@gmail.com>
2025-02-15 15:25:51 +0900

    [FIX] @EnableJpaAuditing 누락 추가

--- saju-backend/src/main/java/com/saju/sajubackend/SajuBackendApplication.java
+++ saju-backend/src/main/java/com/saju/sajubackend/SajuBackendApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SajuBackendApplication {
     private static final Logger logger = LoggerFactory.getLogger(SajuBackendApplication.class);
