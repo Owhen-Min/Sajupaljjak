aa5822817fbbfdfe07422a23d2146ebbb2c938cf
Lee <dlwhdans98@naver.com>
2025-02-18 04:39:49 +0900

    [FIX] Chat.jsx Chats.jsx useWebSocket.js 파일 수정

--- saju-frontend/src/hooks/useWebSocket.js
+++ saju-frontend/src/hooks/useWebSocket.js
@@ -6,29 +6,27 @@ import { useRef } from "react";
 const useWebSocket = () => {
   const [stompClient, setStompClient] = useState(null);
   // const { accessToken } = useAuth();
-   const clientRef = useRef(null);
+  const clientRef = useRef(null);
 
   useEffect(() => {
-    console.log("웹소켓 연결 시도도");
-    // if (!accessToken) return;
-    //웹소켓 연결
+    console.log("웹소켓 연결 시도");
+    //auth로 받아오게 수정 필요
     const accessToken = localStorage.getItem("accessToken");
     console.log("accessToken", accessToken);
     const client = new Client({
       brokerURL: "wss://i12a408.p.ssafy.io/ws",
-      // webSocketFactory: () => new WebSocket("wss://i12a408.p.ssafy.io/ws"),
       reconnectDelay: 5000,
       connectHeaders: {
         Authorization: `Bearer ${accessToken}`,
       },
       onConnect: () => {
-        console.log("웹소켓 연결 성공 111");
+        console.log("웹소켓 연결 성공");
       },
       onDisconnect: () => {
-        console.log("웹소켓 연결 안됨 111");
+        console.log("웹소켓 연결 실패");
       },
       onStompError: (frame) => {
-        console.error("에러러 발생:", frame);
+        console.error("에러 발생:", frame);
       },
     });
 
@@ -42,7 +40,6 @@ const useWebSocket = () => {
     };
   }, []);
 
-  //웹소켓 관리하는 객체 반환환
   return { stompClient };
 };
 
