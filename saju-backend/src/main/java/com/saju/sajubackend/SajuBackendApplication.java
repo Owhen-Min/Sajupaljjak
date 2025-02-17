533d27f7047c983dd50212d6bee609bd871ebe7a
조희주 <sus6an@naver.com>
2025-02-17 18:10:42 +0900

    #21 [CHORE] 불필요한 import문 제거

--- saju-backend/src/main/java/com/saju/sajubackend/SajuBackendApplication.java
+++ saju-backend/src/main/java/com/saju/sajubackend/SajuBackendApplication.java
@@ -3,7 +3,6 @@ package com.saju.sajubackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
