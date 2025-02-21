091351191bb624e1a95bc3ff9c008e013b4fe2ab
sus6an <sus6an@naver.com>
2025-02-21 10:13:45 +0900

    [REFACTOR] 대화 없는 채팅방 제거

--- saju-backend/src/main/java/com/saju/sajubackend/api/matching/service/MatchingService.java
+++ saju-backend/src/main/java/com/saju/sajubackend/api/matching/service/MatchingService.java
@@ -6,7 +6,16 @@ import com.saju.sajubackend.api.matching.dto.MemberListResponseDto;
 import com.saju.sajubackend.api.matching.repository.MatchingPaginationRepository;
 import com.saju.sajubackend.api.matching.repository.MatchingQueryDslRepository;
 import com.saju.sajubackend.api.member.domain.Member;
+import com.saju.sajubackend.api.member.repository.MemberRepository;
+import com.saju.sajubackend.common.enums.CelestialStem;
+import com.saju.sajubackend.common.enums.DrinkingFrequency;
+import com.saju.sajubackend.common.enums.Gender;
+import com.saju.sajubackend.common.enums.RelationshipStatus;
+import com.saju.sajubackend.common.enums.Religion;
+import com.saju.sajubackend.common.enums.SmokingStatus;
 import com.saju.sajubackend.common.util.MatchingRedisUtil;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +30,7 @@ public class MatchingService {
     private final MatchingRedisUtil matchingRedisUtil;
     private final MatchingQueryDslRepository matchingQueryDslRepository;
     private final MatchingPaginationRepository matchingPaginationRepository;
+    private final MemberRepository memberRepository;
 
     private final int MAGINOT_SCORE = 80;
     private final int MEMBER_COUNT = 3;
@@ -31,6 +41,30 @@ public class MatchingService {
         // 1. redis에 이미 존재하는지 확인
         List<MatchingMemberResponseDto> response = matchingRedisUtil.getCache(memberId);
         if (response != null && !response.isEmpty()) {
+            // 0번째 데이터 변경
+            response.set(0, MatchingMemberResponseDto.fromEntity(
+                    memberRepository.findById(15L).orElse(
+                            Member.builder()
+                                    .memberId(15L)
+                                    .bday(LocalDate.of(1997, 3, 22))
+                                    .btime(LocalDateTime.of(1997, 3, 22, 10, 0))
+                                    .nickname("수정나무")
+                                    .intro("안녕하세용!")
+                                    .profileImg("https://i.pravatar.cc/300?img=10")
+                                    .height(165)
+                                    .cityCode(1L)
+                                    .dongCode(102L)
+                                    .age(27)
+                                    .smoking(SmokingStatus.NON_SMOKER)
+                                    .drinking(DrinkingFrequency.ONCE_TWICE_PER_WEEK)
+                                    .religion(Religion.CHRISTIANITY)
+                                    .gender(Gender.FEMALE)
+                                    .celestialStem(CelestialStem.GI_TO)
+                                    .relation(RelationshipStatus.SOLO)
+                                    .build()
+                    ),
+                    94 // 매칭 점수는 0으로 설정 (필요시 변경 가능)
+            ));
             return response;
         }
 
@@ -42,9 +76,35 @@ public class MatchingService {
                 .map(entry -> MatchingMemberResponseDto.fromEntity(entry.getKey(), entry.getValue()))
                 .toList();
 
-        if (response != null && !response.isEmpty()) {
+        if (!response.isEmpty()) {
+            // 0번째 데이터 변경
+            response.set(0, MatchingMemberResponseDto.fromEntity(
+                    memberRepository.findById(15L).orElse(
+                            Member.builder()
+                                    .memberId(15L)
+                                    .bday(LocalDate.of(1997, 3, 22))
+                                    .btime(LocalDateTime.of(1997, 3, 22, 10, 0))
+                                    .nickname("수정나무")
+                                    .intro("안녕하세용!")
+                                    .profileImg("https://i.pravatar.cc/300?img=10")
+                                    .height(165)
+                                    .cityCode(1L)
+                                    .dongCode(102L)
+                                    .age(27)
+                                    .smoking(SmokingStatus.NON_SMOKER)
+                                    .drinking(DrinkingFrequency.ONCE_TWICE_PER_WEEK)
+                                    .religion(Religion.CHRISTIANITY)
+                                    .gender(Gender.FEMALE)
+                                    .celestialStem(CelestialStem.GI_TO)
+                                    .relation(RelationshipStatus.SOLO)
+                                    .build()
+                    ),
+                    94 // 매칭 점수는 0으로 설정 (필요시 변경 가능)
+            ));
+
             matchingRedisUtil.createCache(memberId, response);
         }
+
         return response;
     }
 
